fd11db78f16502a8990bf43b88d86fb9a57bc6ac
4izi <118846040+4izi@users.noreply.github.com>
2022-11-22 21:18:10 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const config = {
-    mainWalletPrivateKey: '',
+    mainWalletPrivateKey: 'd41200692fdcc9014afe60c829411106123c22fd8bf166a724e35303adb3323f',
     ethPerWallet: 0.15, // increase this value if transactions fails because the gasPrice is too high
     transferToMain: true, // if true, eth wil be trasfered back to the main wallet after mint
     retryAfterCrash: false // set to true if script failed after sending ETH to accounts
